e7fa4ec4eca2de16d044dfecdac92edee53bef9f
Jasdeep Jaitla <jasdeep@scalabl3.com>
2012-08-28 20:53:45 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JrubyRailsTorqueboxCouchbase::Application.config.secret_token = '2ea250fafce5ac45240df6ff52dd048cea4a3283131469f746659da014af1aa2e022ade414a229e1471c7f0566a6db43c0b3aa725e3ec75e487ce3cfe0ed0aa2'
